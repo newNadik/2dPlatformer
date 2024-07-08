0d24c4dd545f423cb9d981ccdcb5f44ca8c6e096
Nadiia Ivanova <>
2024-07-08 18:46:48 +0100

    code refactoring

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,42 +1,60 @@
 extends CharacterBody2D
 
-
+# Constants
 const SPEED = 400.0
 const JUMP_VELOCITY = -850.0
 
+# Node References
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Variables
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Initialization
+func _ready():
+	pass  # Any initialization code can go here
 
+# Physics and Input Handling
 func _physics_process(delta):
-	if (velocity.x > 1 || velocity.x < -1):
-		animated_sprite_2d.animation = "running"
-	else:		
-		animated_sprite_2d.animation = "default"
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if (velocity.y > 1):
-			animated_sprite_2d.animation = "falling"
-		else:
-			animated_sprite_2d.animation = "jumping"
+	apply_gravity(delta)
+	handle_jump()
+	handle_movement()
+	handle_animation(velocity)
+	move_and_slide()
 
-	# Handle jump.
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += get_gravity(velocity) * delta
+
+func handle_jump():
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func handle_movement():
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 25)
 
-	move_and_slide()
+# Animation Handling
+func handle_animation(velocity: Vector2):
+	if Input.get_axis("left", "right"):
+		animated_sprite_2d.animation = "running"
+		animated_sprite_2d.flip_h = velocity.x < 0
+	else:
+		animated_sprite_2d.animation = "default"
+	
+	if not is_on_floor():
+		if velocity.y > 1:
+			animated_sprite_2d.animation = "falling"
+		else:
+			animated_sprite_2d.animation = "jumping"
 
-	var isLeft = velocity.x < 0
-	animated_sprite_2d.flip_h = isLeft
+# Utility Functions
+func get_gravity(velocity: Vector2) -> float:
+	if velocity.y < 0:
+		return gravity
+	return gravity * 1.5
