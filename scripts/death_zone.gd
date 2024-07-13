86cb32c6e64338414538d503804944d0a67343ca
Nadiia Ivanova <>
2024-07-13 09:57:08 +0100

    respawn added

--- scripts/death_zone.gd
+++ scripts/death_zone.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+@export var respawn: Node2D
+
 func _on_body_entered(body):
-	if body.name == "Player":  # Ensure the body is the player
-		get_tree().reload_current_scene()
+	if body.name == "Player": 
+		if respawn == null: # Ensure the body is the player
+			get_tree().reload_current_scene()
+		else:
+			body.position = respawn.global_position
+			body.velocity = Vector2.ZERO
