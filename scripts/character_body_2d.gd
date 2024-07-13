d733215150d1b867941ce57e22212f4f84da5070
Nadiia Ivanova <>
2024-07-13 10:53:04 +0100

    walljump implemented

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -2,12 +2,17 @@ extends CharacterBody2D
 
 # Constants (parametrized)
 @export var speed: float = 400.0
+@export var friction: float = 25
 @export var jump_velocity: float = -850.0
 @export var can_double_jump: bool = true
 @export var can_dash: bool = true
 @export var dash_speed: float = 1200.0
 @export var dash_duration: float = 0.2
 @export var dash_cooldown: float = 0.3
+@export var can_wall_jump: bool = true
+@export var wall_jump_pushback: float = 100
+@export var is_wall_sliding: bool = false
+@export var wall_slide_gravity_mult: float = 0.05
 
 # Node References
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -38,6 +43,8 @@ func _physics_process(delta):
 		handle_jump()
 		handle_movement()
 		handle_dash()
+		if can_wall_jump:
+			handle_wall_slide(delta)
 	handle_animation(velocity)
 	move_and_slide()
 
@@ -52,6 +59,12 @@ func handle_jump():
 		if is_on_floor():
 			velocity.y = jump_velocity
 			has_double_jumped = false
+		elif is_on_wall() and can_wall_jump and Input.is_action_pressed("left"):
+			velocity.y = jump_velocity
+			velocity.x = wall_jump_pushback
+		elif is_on_wall() and can_wall_jump and Input.is_action_pressed("right"):
+			velocity.y = jump_velocity
+			velocity.x = -wall_jump_pushback
 		elif can_double_jump and not has_double_jumped:
 			velocity.y = jump_velocity
 			has_double_jumped = true
@@ -63,7 +76,7 @@ func handle_movement():
 			last_direction = direction
 			velocity.x = direction * speed
 		else:
-			velocity.x = move_toward(velocity.x, 0, 25)
+			velocity.x = move_toward(velocity.x, 0, friction)
 
 func handle_dash():
 	if can_dash and Input.is_action_just_pressed("dash") and not is_dashing and dash_cooldown_time <= 0:
@@ -89,6 +102,23 @@ func handle_animation(velocity: Vector2):
 	
 	if is_dashing:
 		animated_sprite_2d.animation = "running"
+
+
+func handle_wall_slide(delta):
+	if is_on_wall() and not is_on_floor():
+		if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+			is_wall_sliding = true
+		else:
+			is_wall_sliding = false
+	else:
+		is_wall_sliding = false
+		
+	if is_wall_sliding:
+		#var slide_gravity = get_gravity(velocity) * wall_slide_gravity_mult
+		var slide_gravity = gravity * wall_slide_gravity_mult
+		velocity.y += slide_gravity * delta
+		velocity.y = min(velocity.y, slide_gravity)
+		
 
 # Utility Functions
 func get_gravity(velocity: Vector2) -> float:
